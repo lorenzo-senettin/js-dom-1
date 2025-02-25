d55bce1080d0fe8b7d55d511982b9a7d562d03b8
lorenzo-senettin <senettin.lorenzo54@gmail.com>
2025-02-25 15:57:34 +0100

    add switch

--- script.js
+++ script.js
@@ -3,5 +3,11 @@ const lampEl = document.querySelector('img');
 const btnEl = document.querySelector('button');
 
 btnEl.addEventListener('click', function () {
-  lampEl.src = './img/yellow_lamp.png'
+  if (lampEl.src.includes('white')) {
+    lampEl.src = './img/yellow_lamp.png'
+    btnEl.innerText = 'TURN OFF'
+  } else {
+    lampEl.src = './img/white_lamp.png'
+    btnEl.innerText = 'TURN ON'
+  }
 })
