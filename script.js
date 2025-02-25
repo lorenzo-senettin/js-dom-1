7869e9ed16f20921c33bd0cb4a0fb784949ea788
lorenzo-senettin <senettin.lorenzo54@gmail.com>
2025-02-25 15:47:30 +0100

    add dom element selection

--- script.js
+++ script.js
@@ -1,31 +1,3 @@
-/* Scrivi una funzione che accetti una stringa e restituisca il numero di vocali contenute al suo interno */
-
-const word = 'javascript';
-
-
-// Dichiara la funzione qui.
-function numVoc(str) {
-
-  let count = 0;
-  const voc = ['a', 'e', 'i', 'o', 'u'];
-  const foundVowels = [];
-  for (let i = 0; i < str.length; i++) {
-    const thisChar = str[i];
-
-    if (voc.includes(thisChar)) {
-      count++;
-      foundVowels.push(thisChar);
-    }
-
-  }
-  return [count, foundVowels];
-
-}
-
-
-// Invoca la funzione qui e stampa il risultato in console
-const vocNumb = numVoc(word);
-console.log(vocNumb, word);
-
-
-//Risultato atteso se si passa 'javascript': 3 (a, a, i)
+// select dom element
+const lampEl = document.querySelector('img')
+const btnEl = document.querySelector('button')
