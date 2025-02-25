401d20b49ee6e1089988879749ceae23f806b77e
lorenzo-senettin <senettin.lorenzo54@gmail.com>
2025-02-25 15:53:51 +0100

    add turn on

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 // select dom element
-const lampEl = document.querySelector('img')
-const btnEl = document.querySelector('button')
+const lampEl = document.querySelector('img');
+const btnEl = document.querySelector('button');
+
+btnEl.addEventListener('click', function () {
+  lampEl.src = './img/yellow_lamp.png'
+})
